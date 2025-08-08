ca7dbb2afc20ff22824aebe5b7e7483e0097ca7f
sushant81074 <sushant94601@gmail.com>
2025-08-08 15:18:27 +0530

    fetch done

--- middlewares/routerMiddleware.ts
+++ middlewares/routerMiddleware.ts
@@ -1,19 +1,24 @@
 import { type NextFunction, type Request, type Response } from "express";
-import crashRouter from "../routes/crash-games/baseCrashRoute"
+import crash from "../routes/crash-games/baseCrashRoute"
 import { notFound } from "../middlewares/notFoundHandler";
 import { DB_GAMES_LIST } from "../db/dbConnect";
 
 const globalRouter = {
-    crashRouter
+    crash
 } as const;
 
 type RouteKey = keyof typeof globalRouter
 
 export const routerMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const app = req.params.app;
-    if (!DB_GAMES_LIST.includes(app)) return notFound(req, res, next);
 
-    const route = globalRouter[app as RouteKey];
+    const category = Object.keys(DB_GAMES_LIST).find(key => DB_GAMES_LIST[key].includes(app));
+    if (!category) return notFound(req, res, next);
+
+    if (!req.body) req.body = { app, category, ...req.query };
+    else req.body = { ...req.body, app, category, ...req.query };
+
+    const route = globalRouter[category as RouteKey];
     if (app && typeof route === "function") return route(req, res, next);
 
     return notFound(req, res, next);
--- services/base.ts
+++ services/base.ts
@@ -1,7 +1,12 @@
-import { GamesDbConnect } from "../db/dbConnect";
+import { gamesDbConnection } from "../db/dbConnect";
 import type { IBetDetailArgs, IBetHistoryArgs } from "../interfaces/service";
 
-export abstract class BaseService extends GamesDbConnect {
+export abstract class BaseService {
+    protected gamesDbConnect = gamesDbConnection;
+
+    protected async getGameDbPool(app: string) {
+        return this.gamesDbConnect.getGameDbPool(app);
+    }
     abstract betHistory(args: IBetHistoryArgs): Promise<any>;
     abstract betDetails(args: IBetDetailArgs): Promise<any>;
 }
