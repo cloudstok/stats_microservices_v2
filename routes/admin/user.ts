3bd721eed072aaec3564452a9f44eda6fc577c02
sushant81074 <sushant94601@gmail.com>
2025-09-23 13:34:20 +0000

    auth placed at signup

--- routes/admin/user.ts
+++ routes/admin/user.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { userInstance as controller } from "../../controllers/admin/user";
+import { auth } from "../../middlewares/authMiddleware";
 
 export const router = Router();
 
 router.route("/signIn").post(controller.signIn);
+router.use(auth)
 router.route("/signUp").post(controller.signUp);
