43e357cf8d05c037e43be221dd529bab79f2c05b
Chinmay Gawali <chinmay1gawali@gmail.com>
2025-09-24 15:58:13 +0530

    Added color 2.0 mapper

--- interceptor/custom/color2.0.ts
+++ interceptor/custom/color2.0.ts
@@ -19,15 +19,17 @@ export class Color2_0 extends ARespMapper {
         return formattedResp;
     }
 
-    history = (resp: any[]) => resp.map(e => {
+    history = (resp: any[]) => resp.map(row => {
         return {
-            lobby_id: e.lobby_id,
-            user_id: e.user_id,
-            operator_id: e.operator_id,
-            bet_amount: e.bet_amount,
-            profit: e.win_amount - e.bet_amount,
-            max_mult: e.max_mult,
-            created_at: e.created_at,
+            lobby_id: row.lobby_id,
+            user_id: row.user_id,
+            operator_id: row.operator_id,
+            totalBetAmount: Number(row.bet_amount).toFixed(2),
+            max_mult: Number(row.max_mult).toFixed(2),
+            userBets: typeof row.userBets === "string" ? JSON.parse(row.userBets) : row.userBets,
+            result: typeof row.result === "string" ? JSON.parse(row.result) : row.result,
+            winAmount: Number(row.win_amount).toFixed(2),
+            created_at: row.created_at
         }
     });
 
@@ -39,25 +41,43 @@ export class Color2_0 extends ARespMapper {
 
         const e = resp[0];
 
-        const betsObj = JSON.parse(e.userBets || "[]")?.reduce(
+        const colorMap: Record<number, string> = {
+            1: "Yellow",
+            2: "White",
+            3: "Purple",
+            4: "Blue",
+            5: "Red",
+            6: "Green"
+        };
+
+        const userBets = typeof e.userBets === "string" ? JSON.parse(e.userBets) : e.userBets;
+
+        const betsObj = (userBets || []).reduce(
             (acc: any, bet: any, index: number) => {
-                acc[`bet_${index + 1}`] = bet;
+                acc[`bet_${index + 1}`] = {
+                    ...bet,
+                    betAmount: Number(bet.betAmount).toFixed(2),
+                    winAmount: Number(bet.winAmount).toFixed(2),
+                    mult: Number(bet.mult).toFixed(2),
+                };
                 return acc;
             },
             {}
         );
 
+        const resultArray = typeof e.result === "string" ? JSON.parse(e.result) : e.result;
+        const result = (resultArray || []).map((num: number) => colorMap[num] || num);
+
         const data: any = {
-            lobby_id: e.lobby_id,
             user_id: e.user_id,
             operator_id: e.operator_id,
-            bet_amount: e.bet_amount,
-            win_amount: e.win_amount,
-            max_mult: e.max_mult,
-            created_at: e.created_at,
+            totalBetAmount: Number(e.bet_amount).toFixed(2),
+            totalWinAmount: Number(e.win_amount).toFixed(2),
             ...betsObj,
+            result: result
         };
 
         return data;
-    };
+    }
+
 }
